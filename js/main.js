fdd219a8c58c4c9b55d8f7c4dcafb9d2b74f09a2
TuyenNguyen <nguyentuyen1322@gmail.com>
2020-03-09 22:53:17 +0700

    done pages

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    var typed = new Typed('#typed',{
+        strings: ["Web Development", "Mobile Development", "Email Newsletter (EDM)", "Wordpress", "ReactJS", "AngularJS", "React Native", "PHP ZEND & Magento"],
+        typeSpeed: 30,
+        backSpeed: 30,
+        backDelay: 2000,
+        startDelay: 1000,
+        loop: true,
+        showCursor: true
+    });
+});
